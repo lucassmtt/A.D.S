bb7b118eef5df294bc29e9021e3e43482ae2225b
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-31 22:54:28 -0300

    Descrição do próximo exercício...

---
+++ C&C++/Aula_01/ex-06.c
@@ -0,0 +1,5 @@
+// Exercício 1
+
+// Desenvolva um programa que leia um valor N inteiro positivo, calcule e mostre o valor de E,
+// conforme a fórmula abaixo:
+// E = 1 + 1/1! + 1/2! + 1/3! + ... + 1/N!
