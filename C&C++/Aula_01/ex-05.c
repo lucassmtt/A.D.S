28b0894bb69e0f0bccf4e6071e3b44aceef9996f
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-31 22:51:01 -0300

    Último do dia...

--- C&C++/Aula_01/ex-05.c
+++ C&C++/Aula_01/ex-05.c
@@ -1,1 +1,21 @@
+// Exercício de aquecimento 5
+// Escreva um programa que pergunta um número ao usuário, e mostra sua tabuada
+// completa (de 1 até 10).
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int numUsuario, cont;
+
+    printf("Digite um número para saber sua tabuada");
+    scanf("%d", &numUsuario);
+
+    for (numUsuario, cont=0; cont < 10; cont++){
+        int soma = 0;
+        soma = numUsuario * cont;
+        printf("%d ", soma);
+    }
+}
